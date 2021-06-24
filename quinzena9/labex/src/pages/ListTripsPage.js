c447358b6b62ce7223831f8beadd6e249cd40ace
unknown <you@example.com>
2021-06-24 20:35:10 -0300

    labeX aula 35

---
+++ quinzena9/labex/src/pages/ListTripsPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ListTripsPage() {
+  return (
+          <div >
+            <h1 >
+              Sydney, 
+              Australia
+            </h1>
+            </div>
+         );
+}
+export default ListTripsPage;
---
+++ quinzena9/labex/src/pages/LoginPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function LoginPage() {
+  return (
+    <div> 
+    </div>
+  );
+}
+export default LoginPage;
