87a7ff1f557b47acd45c961b7caec801aca1b524
unknown <you@example.com>
2021-07-04 22:21:45 -0300

    entrega labeX

--- quinzena9/labex/src/pages/ListTripsPage.js
+++ quinzena9/labex/src/pages/ListTripsPage.js
@@ -4,28 +4,57 @@ import { Link } from "react-router-dom";
 function ListTripsPage() {
   return (
     <div className="mt-10 lg:mt-32 mx-auto container">
-      <h1 className="lg:text-5xl text-2xl text-center f-m-w font-bold">
-        Lista de Viagens
+      <center>
+        <h1 className="lg:text-5xl text-2xl text-center f-m-w font-bold">
+        LISTA DE VIAGENS
+        <br></br>
       </h1>
-      <div className="flex justify-center items-center mt-4">
-        <Link to="/">
-          <button
-            type="submit"
-            className="w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-black shadow-md hover:text-black hover:bg-white focus:outline-none focus:ring-2 mr-4"
-          >
+      </center>
+      <div>
+            <h2>
+              Marte
+            </h2>
+            <p>O quarto planeta a partir do Sol, o segundo menor do Sistema Solar. Batizado em homenagem ao deus romano da guerra, 
+            como o "Planeta Vermelho".
+            </p>
+            <h2>
+              Jupter
+            </h2>
+            <p>O maior planeta do Sistema Solar, tanto em diâmetro quanto em massa, e é o quinto mais próximo do Sol.
+            </p>
+            <h2>
+              Saturno
+            </h2>
+            <p>O segundo maior planeta do nosso sistema solar, tem um diâmetro nove vezes maior que o da Terra e é composto, em maior parte, por hidrogênio.
+            </p>
+            <h2>
+              Urano
+            </h2>
+            <p>Terceiro maior planeta do sistema solar e sétimo planeta a partir do sol, ele é gasoso que apresenta médias de temperatura de -185°C
+            </p>
+            <h2>
+              Netuno
+            </h2>
+            <p> O mais distante do sol e o quarto maior em tamanho,um planeta gasoso, formado por hidrogênio, hélio, amônio, metano e água.
+            </p>
+            
+      <div>
+        <center>
+          <Link to="/">
+          <button>
             <span className="w-full">Voltar</span>
           </button>
         </Link>
         <Link to="/viagens/inscrever">
-          <button
-            type="submit"
-            className="w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-black shadow-md hover:text-black hover:bg-white focus:outline-none focus:ring-2 ml-4"
-          >
+          <button>
             <span className="w-full">Inscrever-se</span>
           </button>
         </Link>
-      </div>
+        </center>
+     </div>
     </div>
+   </div>
+
   );
 }
 export default ListTripsPage;
